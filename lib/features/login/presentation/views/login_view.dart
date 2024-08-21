d58b951785c2c47c9c7bd94419baafe0878392bb
rahmagamal217 <rahmagm002@gmail.com>
2024-08-21 13:59:43 +0300

    editing login view

--- lib/features/login/presentation/views/login_view.dart
+++ lib/features/login/presentation/views/login_view.dart
@@ -1,59 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sign_in_button/sign_in_button.dart';
-import 'package:signup/features/login/data/services/google_api.dart';
+import 'package:signup/features/login/presentation/controller/bloc/login_bloc.dart';
+import 'package:signup/features/login/presentation/controller/bloc/login_state.dart';
 
-class LoginView extends StatefulWidget {
+import '../controller/bloc/login_event.dart';
+
+class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
-  State<LoginView> createState() => _LoginViewState();
-}
-
-class _LoginViewState extends State<LoginView> {
-  GoogleSignInAccount? _currentUser;
-  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: _currentUser == null
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SignInButton(
-                      Buttons.google,
-                      onPressed: () async {
-                        _currentUser = await GoogleApi.login();
-                        setState(() {});
-                      },
-                      text: 'Login with Google',
-                    ),
-                    // ElevatedButton.icon(
-                    //   icon: Image.asset(
-                    //     'assets/images/google.png',
-                    //     height: 24,
-                    //     width: 24,
-                    //   ),
-                    //   label: const Text('Login with Google'),
-                    //   onPressed: () {},
-                    // ),
-                    const SizedBox(height: 16),
-                    SignInButton(
-                      Buttons.facebook,
-                      onPressed: () {},
-                      text: 'Login with Facebook',
-                    ),
-                  ],
-                )
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Welcome ${_currentUser!.displayName!}'),
-                    const SizedBox(height: 16),
-                    Text(_currentUser!.email),
-                  ],
-                ),
+    return BlocProvider(
+      create: (context) => LoginBloc(),
+      child: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: BlocBuilder<LoginBloc, LoginState>(
+              builder: (context, state) {
+                if (state is Initial || state is Unauthenticated) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SignInButton(
+                        Buttons.google,
+                        onPressed: () {
+                          context
+                              .read<LoginBloc>()
+                              .add(const LoginEvent.loginWithGoogle());
+                        },
+                        text: 'Login with Google',
+                      ),
+                      // ElevatedButton.icon(
+                      //   icon: Image.asset(
+                      //     'assets/images/google.png',
+                      //     height: 24,
+                      //     width: 24,
+                      //   ),
+                      //   label: const Text('Login with Google'),
+                      //   onPressed: () {},
+                      // ),
+                      const SizedBox(height: 16),
+                      SignInButton(
+                        Buttons.facebook,
+                        onPressed: () {
+                          context
+                              .read<LoginBloc>()
+                              .add(const LoginEvent.loginWithFacebook());
+                        },
+                        text: 'Login with Facebook',
+                      ),
+                    ],
+                  );
+                } else if (state is Loading) {
+                  return const CircularProgressIndicator();
+                } else if (state is Authenticated) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Welcome ${state.user.displayName!}'),
+                      const SizedBox(height: 16),
+                      Text(state.user.email),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          context
+                              .read<LoginBloc>()
+                              .add(const LoginEvent.logout());
+                        },
+                        child: const Text('Logout'),
+                      ),
+                    ],
+                  );
+                } else if (state is Error) {
+                  return Text('Error: ${state.message}');
+                } else {
+                  return const Text('Something went wrong');
+                }
+              },
+            ),
+          ),
         ),
       ),
     );
